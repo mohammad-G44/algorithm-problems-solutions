09fa1d2e6c68bd743a9a8ade00fb93c2acae7afa
Mohammad Ghaddar <133534953+mohammad-G44@users.noreply.github.com>
2025-01-04 21:53:18 +0200

    Create  (A) MEX Table.cpp

---
+++ (A) MEX Table.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+ 
+ 
+ 
+ 
+int main(){
+int t;
+cin>>t;
+while(t--){
+  ll n,k;
+  cin>>n>>k;
+ int m=max(n,k);
+ cout<<m+1<<endl;
+ 
+ 
+}
+ 
+ 
+ 
+ 
+return 0;}
