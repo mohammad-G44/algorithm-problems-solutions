31eb591aed097fc2939c0f7be7a879501a87a8a7
Mohammad Ghaddar <133534953+mohammad-G44@users.noreply.github.com>
2025-01-08 15:05:13 +0200

    Create 1946A - Median of an Array.cpp

---
+++ 1946A - Median of an Array.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using i64 = long long;
+
+void solve() {
+    int n;
+    std::cin >> n;
+    std::vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        std::cin >> a[i];
+    }
+    std::sort(a.begin(), a.end());
+    int p = (n + 1) / 2 - 1;
+    int res = std::count(a.begin() + p, a.end(), a[p]);
+    std::cout << res << "\n";
+}
+
+signed main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int t = 1;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+}
