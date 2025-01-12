e210b06602e53eac3bab068e114cc08575b0aa20
Mohammad Ghaddar <133534953+mohammad-G44@users.noreply.github.com>
2025-01-12 23:05:12 +0200

    Create (A) Two Frogs.cpp

---
+++ (A) Two Frogs.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <cmath>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin>>t;
+    while (t--){
+    long long m,a,b;
+    cin >> m>>a>>b;
+   
+ 
+if(abs(a-b)%2==1)
+cout<<"NO"<<endl;
+else cout<<"YES"<<endl;
+ 
+    
+    }
+    return 0;
+}
