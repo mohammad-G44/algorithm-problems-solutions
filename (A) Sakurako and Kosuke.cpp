5effc2798f02b824370dc90f97242986bbed2886
Mohammad Ghaddar <133534953+mohammad-G44@users.noreply.github.com>
2025-01-14 22:28:35 +0200

    Create  (A) Sakurako and Kosuke.cpp

---
+++ (A) Sakurako and Kosuke.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <cmath>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin>>t;
+    while (t--){
+    long long m,a,b;
+    cin >> m;
+   
+ 
+if(m%2==1)
+cout<<"Kosuke"<<endl;
+else cout<<"Sakurako"<<endl;
+ 
+    
+    }
+    return 0;
+}
