1d0556d3214bdc82436d1c13dd2ea49eadde5187
Mohammad Ghaddar <133534953+mohammad-G44@users.noreply.github.com>
2025-01-22 23:11:40 +0200

    Create  (A) Minimal Coprime.cpp

---
+++ (A) Minimal Coprime.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long l,r;
+        cin>>l>>r;
+        if(r==1)cout<<1<<endl;
+        else cout<<r-l<<endl;
+        
+    }
+    
+    
+    
+    return 0;
+}
